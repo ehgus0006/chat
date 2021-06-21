4095ea52ad17e4c229d4f300244cf1a361a08e79
ehgus0006 <74715118+ehgus0006@users.noreply.github.com>
2021-06-21 14:33:08 +0900

    Chating Test

---
+++ src/main/java/com/example/chat/chat/config/WebSocketConfig.java
@@ -0,0 +1,21 @@
+package com.example.chat.chat.config;
+
+import com.example.chat.chat.handler.SocketHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@EnableWebSocket
+@Configuration
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Autowired
+    SocketHandler socketHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(socketHandler, "/chating");
+    }
+}
